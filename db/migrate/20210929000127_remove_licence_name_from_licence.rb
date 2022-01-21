202df3ef70c4cedd983c5c760e3a2103def00c4f
ramirezmansillacar <carlo.ss.smx@gmail.com>
2022-01-21 03:08:30 -0300

    fx

--- db/migrate/20210929000127_remove_licence_name_from_licence.rb
+++ db/migrate/20210929000127_remove_licence_name_from_licence.rb
@@ -1,5 +1,5 @@
 class RemoveLicenceNameFromLicence < ActiveRecord::Migration[6.1]
   def change
-    remove_column :licences, :licence_name, :reference
+    # remove_column :licences, :licence_name, :reference
   end
 end
